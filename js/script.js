880a303d26c8a4c3a73e7076aee15faf55dab5d8
Haiza-Shin <goldendwafi@gmail.com>
2024-08-23 14:14:00 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const typed = new Typed('.multiple-text',{
+    strings:['Front-End Web Designer', 'Fluent English Speaker','Video Editor','Leader','Back-End Web Developer' ],
+    typeSpeed:55,
+    backSpeed:55,
+    backDelay:100,
+    loop:true,
+});
